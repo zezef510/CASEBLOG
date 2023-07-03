23a552b61a59d0449926538b3f787ad1ee215db0
nguyenquynh46 <nguyenthiquynhhvnh93@gmail.com>
2023-07-04 05:15:01 +0700

    xong update

--- controller/productController.js
+++ controller/productController.js
@@ -4,19 +4,19 @@ import qs from "qs";
 import url from "url";
 
 class ProductController {
+    ;
 
     showAll(req, res) {
         let data = '';
         req.on('data', dataRaw => {
             data += dataRaw;
         })
-        req.on('end',  () => {
+        req.on('end', () => {
             if (req.method === 'GET') {
                 showList(req, res);
             } else {
                 data = qs.parse(data);
-                console.log(data)
-                productService.save(data). then(()=>{
+                productService.save(data).then(() => {
                     showList(req, res);
                 })
 
@@ -26,9 +26,34 @@ class ProductController {
 
     showFormEdit(req, res) {
         fs.readFile('view/product/edit.html', 'utf-8', (err, stringHTML) => {
-            let urlObject = url.parse(req.url, true)
-            res.write(stringHTML);
-            res.end();
+            let data = '';
+            req.on('data', dataRaw => {
+                data += dataRaw;
+            })
+            req.on('end', () => {
+                if (req.method === 'GET') {
+                    let urlObject = url.parse(req.url, true);
+
+                    productService.findById(urlObject.query.idEdit).then((product) => {
+
+                        stringHTML = stringHTML.replace('{id}', product.id)
+                        stringHTML = stringHTML.replace('{name}', product.name)
+                        stringHTML = stringHTML.replace('{price}', product.price)
+                        stringHTML = stringHTML.replace('{quantity}', product.quantity)
+                        stringHTML = stringHTML.replace('{image}', product.image)
+                        res.write(stringHTML);
+                        res.end();
+                    })
+                } else {
+                    data = qs.parse(data);
+                    productService.update(data).then(() => {
+                        showList(req, res);
+                    })
+
+                }
+            })
+
+
         })
     }
 
@@ -45,9 +70,9 @@ class ProductController {
 function showList(req, res) {
     fs.readFile('view/product/list.html', 'utf-8', (err, stringHTML) => {
         let str = '';
-        productService.findAll().then((products)=> {
+        productService.findAll().then((products) => {
             for (const product of products) {
-                str+=`<h3>${product.name}</h3>`
+                str += `<h3>${product.name}  <a onclick="return window.confirm('Are you sure you want to edit')" href="/edit-product?idEdit=${product.id}"><button>Edit</button></a></h3>`
             }
             stringHTML = stringHTML.replace('{list}', str)
             res.write(stringHTML);
--- service/productService.js
+++ service/productService.js
@@ -12,7 +12,6 @@ class ProductService {
                 if (err) {
                     reject(err)
                 } else {
-                    console.log(products)
                     resolve(products)
                 }
             })
@@ -25,14 +24,35 @@ class ProductService {
                 if (err) {
                     reject(err)
                 } else {
-                    console.log(products)
                     resolve(products)
                 }
             })
         })
     }
 
+
     findById(id) {
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query(`select * from product where id = ${id}`,(err, products) => {
+                if(err){
+                    reject(err)
+                } else {
+                    resolve(products[0])
+                }
+            })
+        })
+    }
+    update (product){
+        return new Promise((resolve, reject) => {
+            connection.getConnection().query(`update product Set name ='${product.name}',price =${product.price},quantity =${product.quantity},image ='${product.image}' where id= ${product.id}`,(err, products) => {
+                if(err){
+                    reject(err)
+                } else {
+                    console.log(`update thanh cong`)
+                    resolve(products)
+                }
+            })
+        })
     }
 }
 
